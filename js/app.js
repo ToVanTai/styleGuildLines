06e4c42d9ba6e0ada33b34e73e280159064b5a82
tovantai <tovantaidz2001@gmail.com>
2022-11-29 14:39:09 +0700

    implement exercise 9

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//handle toast messenger
+//start handle toast messenger
 var listToastMessenger = document.querySelectorAll(".m-toast-messenger-container .m-toast-messenger-item")
 var timeStart = 3000
 listToastMessenger.forEach((item, index) => {
@@ -9,3 +9,35 @@ listToastMessenger.forEach((item, index) => {
     item.style.display = "none"
   })
 })
+//end handle toast messenger
+
+//start handle popup 
+var showPopup = function (type) {
+  let popupElm = document.querySelector(".m-popup")
+  let overlayElm = document.querySelector(".m-overlay")
+  overlayElm.classList.add("open")
+  popupElm.className = "m-popup open"
+  switch (type) {
+    case "warning":
+      popupElm.classList.add("warning")
+      break;
+    case "danger":
+      popupElm.classList.add("danger")
+      break;
+    case "notify":
+      popupElm.classList.add("notify")
+      break;
+    default:
+      popupElm.classList.add("notify")
+      break;
+  }
+}
+var closePopup = function () {
+  console.log("close");
+  let popupElm = document.querySelector(".m-popup")
+  let overlayElm = document.querySelector(".m-overlay")
+  popupElm.classList.remove("open")
+  overlayElm.classList.remove("open")
+}
+document.querySelector(".m-overlay").addEventListener("click", closePopup)
+//end handle popup
